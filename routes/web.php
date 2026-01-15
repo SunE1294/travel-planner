09734daee0735591d1330ce658360e2fdb3dfc86
SunE1294 <shamimulislamsunny@gmail.com>
2026-01-15 15:01:43 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TravelController; ো
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/api/generate-plan', [TravelController::class, 'getPlan']);
